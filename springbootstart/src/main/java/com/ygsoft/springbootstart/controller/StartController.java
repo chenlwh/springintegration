7dc89cc192a0775d8b0119ac9e2c94263587d9bb
chenlin3 <chenlin3@ygsoft.com>
2018-12-05 12:05:25 +0800

    data

--- springbootstart/src/main/java/com/ygsoft/springbootstart/controller/StartController.java
+++ springbootstart/src/main/java/com/ygsoft/springbootstart/controller/StartController.java
@@ -1,20 +1,31 @@
 package com.ygsoft.springbootstart.controller;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ygsoft.springbootstart.entity.ReleaseData;
+import com.ygsoft.springbootstart.service.ReleaseDataService;
+
 @RestController
 @RequestMapping("/rest/start")
 public class StartController {
+	@Autowired
+	private ReleaseDataService releaseDataService;
+	
     @RequestMapping(value = "/nodata", method = RequestMethod.POST)
     public Map<String, Object> nodata() {
         Map<String, Object> result = new HashMap<String, Object>();
         try {
+        	ReleaseData data = new ReleaseData();
+        	data.setAmount(new BigDecimal(500));
+        	releaseDataService.save(data);
             result.put("suc", "yes");
             result.put("data", "success");
         } catch (Exception e) {
--- springbootstart/src/main/java/com/ygsoft/springbootstart/service/impl/ReleaseDataServiceImpl.java
+++ springbootstart/src/main/java/com/ygsoft/springbootstart/service/impl/ReleaseDataServiceImpl.java
@@ -1,16 +1,20 @@
 package com.ygsoft.springbootstart.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ygsoft.springbootstart.entity.ReleaseData;
 import com.ygsoft.springbootstart.service.ReleaseDataService;
+import com.ygsoft.springbootstart.service.repository.ReleaseDataRepository;
 
 @Service
 public class ReleaseDataServiceImpl implements ReleaseDataService {
 
+	@Autowired
+	private ReleaseDataRepository releaseDataRepository;
 	@Override
 	public void save(ReleaseData data) {
-		// TODO Auto-generated method stub
+		releaseDataRepository.save(data);
 
 	}
 
